7f87e2e6d5859bd6597fa630f56ed487fbe0eef8
matekoszo <50274997+matekoszo@users.noreply.github.com>
2020-04-16 09:28:25 +0200

    Add files via upload

--- Film.java
+++ Film.java
@@ -1,8 +1,31 @@
 package sample;
 
 public class Film {
-    private String filmname;
-    private int age;
-    private int price;
+    private String filname;
+    private short fsk;
+    private short price;
     private String shortDescription;
+
+    public Film(String filname, short fsk, short prince, String shortDescription) {
+        this.filname = filname;
+        this.fsk = fsk;
+        this.price = prince;
+        this.shortDescription = shortDescription;
+    }
+
+    public String getFilname() {
+        return filname;
+    }
+
+    public short getFsk() {
+        return fsk;
+    }
+
+    public short getPrice() {
+        return price;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
 }
